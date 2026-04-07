380380753686159bc289d43c79002d56196ff656
Talha <pateltalha934@gmail.com>
2026-04-07 09:56:04 +0530

    Initializing Project

---
+++ TaskMgmt/src/test/java/com/springboot/taskmgmt/TaskMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.taskmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
